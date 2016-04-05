7ad0663b44a4629a0855f00dc29248fef6010880
Myles Braithwaite <me@mylesbraithwaite.com>
2016-04-05 14:27:00 -0400

    Expand a bunch of URLs.

--- tw2txt/frontend.py
+++ tw2txt/frontend.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 from flask import Blueprint, Response, url_for, current_app as app
 
 from flask_httpauth import HTTPDigestAuth
@@ -36,32 +38,56 @@ def index():
     twtxt = []
 
     for tweet in get_tweets(app.config['TWITTER_USERNAME']):
+        timestamp = pytz.utc.localize(tweet.created_at).isoformat()
+
         if tweet.retweeted:
-            screen_name = tweet.retweeted_status.author.screen_name
-            response = "{0}\tRT @<{1} {2}{3}> {4}\n".format(
-                pytz.utc.localize(tweet.created_at).isoformat(),
-                screen_name,
-                app.config['HEROKU_URL'].rstrip('/'),
-                url_for('frontend.get_user_timeline', username=screen_name),
-                tweet.retweeted_status.text.replace('\n', ' ¶ '))
+            retweet_screen_name = tweet.retweeted_status.author.screen_name
+            retweet_url = urljoin(app.config['HEROKU_URL'],
+                                  url_for('frontend.get_user_timeline',
+                                          username=retweet_screen_name))
+            text = tweet.retweeted_status.text
         else:
-            response = "{0}\t{1}\n".format(
-                        pytz.utc.localize(tweet.created_at).isoformat(),
-                        tweet.text.replace('\n', ' ¶ '))
+            text = tweet.text.replace('\n', ' ¶ ')
+
+        for url in tweet.entities.get('urls', []):
+            text = text.replace(url['url'], url['expanded_url'])
+
+        for media in tweet.entities.get('media', []):
+            text = text.replace(media['url'], media['media_url_https'])
+
+        for user_mention in tweet.entities.get('user_mentions', []):
+            user_url = urljoin(app.config['HEROKU_URL'],
+                               url_for('frontend.get_user_timeline',
+                                       username=user_mention['screen_name']))
+            mention = "@<{0} {1}>".format(user_mention['screen_name'],
+                                          user_url)
+
+            text = text.replace("@{0}".format(user_mention['screen_name']),
+                                mention)
+
+        if tweet.retweeted:
+            response_text = text.replace('\n', ' ¶ ')
+            response = "{0}\tRT @<{1} {2}> {3}\n".format(timestamp,
+                                                         retweet_screen_name,
+                                                         retweet_url,
+                                                         response_text)
+        else:
+            response = "{0}\t{1}\n".format(timestamp,
+                                           text.replace('\n', ' ¶ '))
+
         twtxt.append(response)
 
     return Response(''.join(twtxt), mimetype='text/plain')
 
 
 @frontend.route('/user/<username>')
-@auth.login_required
 def get_user_timeline(username):
     twtxt = []
 
     for tweet in get_tweets(username):
-        response = "{0}\t{1}\n".format(
-                    pytz.utc.localize(tweet.created_at).isoformat(),
-                    tweet.text.replace('\n', ' '))
+        timestamp = pytz.utc.localize(tweet.created_at).isoformat()
+        response = "{0}\t{1}\n".format(timestamp,
+                                       tweet.text.replace('\n', ' '))
         twtxt.append(response)
 
     return Response(''.join(twtxt), mimetype='text/plain')
